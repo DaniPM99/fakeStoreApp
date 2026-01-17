88a1b9453476b9e60911c9b4d2dbc403e01ce5a4
Daniel Parra Martin <dparramartin99@gmail.com>
2026-01-17 21:33:04 +0100

    Initial iOS project setup

---
+++ fakeStoreApp/fakeStoreApp/fakeStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  fakeStoreAppApp.swift
+//  fakeStoreApp
+//
+//  Created by Daniel Parra Martin on 17/1/26.
+//
+
+import SwiftUI
+
+@main
+struct fakeStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
